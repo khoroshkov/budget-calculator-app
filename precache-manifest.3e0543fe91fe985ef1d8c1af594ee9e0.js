d60b742491574678aae7a94bef23710732615b88
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2020-07-05 16:47:04 +0300

    Updates

---
+++ precache-manifest.3e0543fe91fe985ef1d8c1af594ee9e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee01ec8b6fcad15dd01d5b196f0ab18e",
+    "url": "/budget-calculator-app/index.html"
+  },
+  {
+    "revision": "946c7603a8325b35d33a",
+    "url": "/budget-calculator-app/static/css/main.3ed5da4a.chunk.css"
+  },
+  {
+    "revision": "463451a60ff540280676",
+    "url": "/budget-calculator-app/static/js/2.cd5ec5fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-calculator-app/static/js/2.cd5ec5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946c7603a8325b35d33a",
+    "url": "/budget-calculator-app/static/js/main.0ad2fb35.chunk.js"
+  },
+  {
+    "revision": "1d1b9b69fbdd07a88127",
+    "url": "/budget-calculator-app/static/js/runtime-main.2df10263.js"
+  },
+  {
+    "revision": "8f6c658d5a6b51ce5eaaaff6a998af1b",
+    "url": "/budget-calculator-app/static/media/pattern.8f6c658d.svg"
+  }
+]);
